deb1a4e6546ab50c5221e4d9bf8f9f6c42ce9d63
Chris <babalau99@mail.ru>
2021-08-02 19:14:24 +0800

    Третье задание 3

--- main_folder/script.js
+++ main_folder/script.js
@@ -1,14 +1,32 @@
-let num = 266219,
-    numStr = num.toString(),
-    product = 1,
-    numArr = numStr.split('');
+let lang = prompt("Select ru / en", "ru");
+const ruD = ["понедельник", "вторник","среда","четверг","пятница","суббота", "восскресенье"];
+const enD = ["monday", "tuesday", "wednesday", "thrusday", "friday", "saturday" , "sunday"];
+const comD = [[ruD], [enD]];
+let ans;
 
-for (let i=0; i< numStr.length; i++){
-    num = +numArr[i];
-    product *= num;
+// Method if
+if (lang === 'ru'){
+    console.log(ruD);
+} else if (lang === 'en') {
+    console.log(enD);
+}
+// Method Switch-case
+
+switch (lang) {
+    case "ru":
+        console.log(ruD);
+        break;
+    case "en": 
+        console.log(enD);
+        break;
 }
 
-console.log(product);
+// Method Multidimensional Array
 
-let cuboid = product ** 3;
-console.log(cuboid.toString().substr(0, 2));
+lang === "ru" ? console.log(comD[0]) : console.log(comD[1]);
+
+// 2nd Excersise
+
+const namePerson = prompt("имя", 'Артем');
+namePerson === 'Артем' ? ans = "директор" : namePerson === "Максим" ? ans = "преподаватель" : ans = "студент";
+console.log(ans);
