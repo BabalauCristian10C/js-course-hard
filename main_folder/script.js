6d8b7fa19027198be73bc8c1e5f05ec35237db4d
Chris <babalau99@mail.ru>
2021-08-03 20:54:44 +0800

    Фикс

--- main_folder/script.js
+++ main_folder/script.js
@@ -1,32 +1,16 @@
 'use strict';
 
-let argument = "123456789112939321932832189821838213871238231893128913813298918891382138128218",
-    argumentLenght = lenghtArgument(argument);
+let argument = "123456789112939321932832189821838213871238231893128913813298918891382138128218";
 
-
-function lenghtArgument (argument) {
-    let index = 0;
-    for (let i = 0; i< 100; i++){
-        if (typeof argument[i] !== "undefined"){
-            index++;
-        }
-        else {
-            break;
-        }
-    }
-    return index;
-}
-
-
-const argumentControl = function(argument, argumentLenght) {
+const argumentControl = function(argument) {
     if (typeof argument !== "string"){
         return "Argument is not a string";
     } else {
         argument = argument.trim();
-        if (argumentLenght > 30) {
+        if (argument.length> 30) {
             argument = argument.substr(0, 31) + "...";
         }
         return argument;
     }
 };
-console.log(argumentControl(argument, argumentLenght));
+console.log(argumentControl(argument));
