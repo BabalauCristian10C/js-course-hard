05bbfa21ad546c258e3d302da1a6cc5c61130fbd
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-10 19:32:31 +0300

    Timer

--- main_folder/script.js
+++ main_folder/script.js
@@ -1,25 +1,50 @@
 'use strict';
 
-const styleBold = 'font-weight:bold; ';
-const styleCursive = 'font-style: italic;';
-const styleCommon = styleBold + styleCursive;
+let textBoxA = document.querySelector(".current-date"),
+    textBoxB = document.getElementById("textBoxB"),
+    weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    month= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+    
+    function setDateCommon (){
+        setInterval(function () {
+                let currentDate = new Date(),
+                    currDay = weekDays[currentDate.getDay()],
+                    currMonth = month[currentDate.getMonth()],
+                    dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minutes " + currentDate.getSeconds() + " seconds",
+                    dateB,
+                    date = currentDate.getDate(),
+                    curmonth = currentDate.getMonth(),
+                    year = currentDate.getFullYear(),
+                    hours = currentDate.getHours(),
+                    minutes = currentDate.getMinutes(),
+                    seconds = currentDate.getSeconds(),
+                    dateArr = [date, curmonth, year,hours, minutes, seconds];
+                    (function (){      
+                        if (currentDate.getSeconds() === 1) {
+                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minutes " + currentDate.getSeconds() + " second";
+                        } else if (currentDate.getMinutes() === 1) {
+                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minute " + currentDate.getSeconds() + " seconds";
+                        } else if (currentDate.getHours() === 1) {
+                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hour " + currentDate.getHours() + " minute " + currentDate.getSeconds() + " seconds";
+                        }
+                        textBoxA.innerHTML = dateA ;
+                    }());
 
-let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    date = new Date(),
-    todayDate = date.getDay();
-
-
-weekDays.forEach(function(item, i, array){
-    if (i === 0 || i === 6) {
-        if (todayDate === i){
-            console.log(`%c ${item}`, styleCommon);
-        } else {
-            console.log(`%c ${item}`, styleCursive);
-        }
-    }else if (todayDate === i){
-        console.log(`%c ${item}`, styleBold);
+                    (function (){
+                        dateArr.forEach(function(item, i){
+                            if (item < 10) {
+                                dateArr[i]= "0"+item;
+                                console.log(dateArr);
+                            } else {
+                                return item;
+                            }
+                        });
+                        dateB = ` ${dateArr[0]}.${dateArr[1]}.${dateArr[2]} - ${dateArr[3]}:${dateArr[4]}:${dateArr[5]}`;    
+                        textBoxB.innerHTML = dateB ;
+                    }());
+                    }, 1000);
     }
-    else{
-        console.log(item);
-    }
-});
+
+setDateCommon();
+
