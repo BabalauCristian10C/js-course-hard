832d52405bed20ac53e5ddbd63ebc308a01ffab4
Chris Babalau <chrisbabalau@Chriss-MacBook-Air.local>
2021-08-14 11:11:24 +0300

    OK

--- main_folder/script.js
+++ main_folder/script.js
@@ -1,50 +1,22 @@
 'use strict';
 
-let textBoxA = document.querySelector(".current-date"),
-    textBoxB = document.getElementById("textBoxB"),
-    weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    month= ["January","February","March","April","May","June","July",
-            "August","September","October","November","December"];
-    
-    function setDateCommon (){
-        setInterval(function () {
-                let currentDate = new Date(),
-                    currDay = weekDays[currentDate.getDay()],
-                    currMonth = month[currentDate.getMonth()],
-                    dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minutes " + currentDate.getSeconds() + " seconds",
-                    dateB,
-                    date = currentDate.getDate(),
-                    curmonth = currentDate.getMonth(),
-                    year = currentDate.getFullYear(),
-                    hours = currentDate.getHours(),
-                    minutes = currentDate.getMinutes(),
-                    seconds = currentDate.getSeconds(),
-                    dateArr = [date, curmonth, year,hours, minutes, seconds];
-                    (function (){      
-                        if (currentDate.getSeconds() === 1) {
-                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minutes " + currentDate.getSeconds() + " second";
-                        } else if (currentDate.getMinutes() === 1) {
-                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hours " + currentDate.getHours() + " minute " + currentDate.getSeconds() + " seconds";
-                        } else if (currentDate.getHours() === 1) {
-                            dateA = "Today is " + currDay + ", " + currentDate.getDate() + " " + currMonth + " " + currentDate.getFullYear()+ ", "+ currentDate.getHours() + " hour " + currentDate.getHours() + " minute " + currentDate.getSeconds() + " seconds";
-                        }
-                        textBoxA.innerHTML = dateA ;
-                    }());
+const body = document.body,
+    button = document.querySelector("button");
 
-                    (function (){
-                        dateArr.forEach(function(item, i){
-                            if (item < 10) {
-                                dateArr[i]= "0"+item;
-                                console.log(dateArr);
-                            } else {
-                                return item;
-                            }
-                        });
-                        dateB = ` ${dateArr[0]}.${dateArr[1]}.${dateArr[2]} - ${dateArr[3]}:${dateArr[4]}:${dateArr[5]}`;    
-                        textBoxB.innerHTML = dateB ;
-                    }());
-                    }, 1000);
+let bodyProperties = {
+    bgc: "#fff",
+    bgGenerate: function(){
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        bodyProperties.bgc = '#' + n.slice(0, 6);
+        function setColor (){
+            body.style.backgroundColor = bodyProperties.bgc;
+            document.querySelector("#hex-code").innerHTML = bodyProperties.bgc;
+        }
+        setColor();
     }
+};
 
-setDateCommon();
 
+
+
+button.addEventListener("click", bodyProperties.bgGenerate);
